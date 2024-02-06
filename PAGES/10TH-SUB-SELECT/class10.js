4a540f0436bafa4a51ee9fa107a4588942c69d2d
Agrawal Tanmay Shravanbhai <125138805+WinnersChoice@users.noreply.github.com>
2024-02-06 22:37:04 +0530

    Delete PAGES directory

--- PAGES/10TH-SUB-SELECT/class10.js
+++
@@ -1,6 +0,0 @@
-// class10.js
-function loadChapterSelection(subject) {
-    window.location.href = `chapter-selection.html?subject=${subject}`;
-}
-
-// "/PAGES/CHAPTER-SELECTION/chapter-selection.html"
--- PAGES/PRIVACY-POLICY/privacy-policy.js
+++
@@ -1,8 +0,0 @@
-// function goBack() {
-//     if (document.referrer) {
-//         window.location.href = document.referrer;
-//     } else {
-//         // If there is no referrer, go to the home page
-//         window.location.href = 'index.html';
-//     }
-// }
